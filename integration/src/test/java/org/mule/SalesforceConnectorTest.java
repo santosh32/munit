d135a1dd8c036c7b89454dfc66d98fd570d05755
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-02-25 15:52:07 -0300

    adding integration

---
+++ integration/src/test/java/org/mule/SalesforceConnectorTest.java
@@ -0,0 +1,12 @@
+package org.mule;
+
+
+import org.mule.munit.AbstractMuleSuite;
+
+public class SalesforceConnectorTest extends AbstractMuleSuite {
+    @Override
+    public String getConfigResources() {
+        return "mule-config.xml, test-config.xml";
+    }
+
+}
