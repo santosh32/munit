5cc9afbca1018eaa211e91f27a45b736c656ae88
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-06-22 01:15:18 -0300

    remove files

--- tools/maven-plugin/target/apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.mule", "org/mule/package-summary.html", [ [ "Classes", null, [ [ "MUnitMojo", "org/mule/MUnitMojo.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- tools/maven-plugin/target/apidocs/java/lists.js
+++
@@ -1,5 +0,0 @@
-var DATA = [
-      { id:0, label:"org.mule", link:"java/org/mule/package-summary.html", type:"package" },
-      { id:1, label:"org.mule.MUnitMojo", link:"java/org/mule/MUnitMojo.html", type:"class" }
-
-    ];
