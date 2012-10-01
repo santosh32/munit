9bd8f1934583cff21e6d2dd72355fc8d41fb02e8
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-10-01 12:29:01 -0300

    remove assert module

--- assert/src/main/java/org/mule/munit/MTest.java
+++
@@ -1,8 +0,0 @@
-package org.mule.munit;
-
-public class MTest extends AbstractMuleSuite {
-    @Override
-    public String getConfigResources() {
-        return System.getProperty("munit.resource");
-    }
-}
