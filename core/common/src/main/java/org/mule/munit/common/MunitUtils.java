bfaf0170fbd75fe230c9800b85aff2a929a60497
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-11-04 17:56:22 -0300

    adding munti utils

---
+++ core/common/src/main/java/org/mule/munit/common/MunitUtils.java
@@ -0,0 +1,58 @@
+package org.mule.munit.common;
+
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.munit.common.mocking.NonDefinedPayload;
+import org.mule.transport.NullPayload;
+
+import java.util.List;
+
+public class MunitUtils {
+
+    public static void changeMessage(DefaultMuleMessage returnMuleMessage, DefaultMuleMessage message) {
+
+        Object payload = returnMuleMessage.getPayload();
+        if ( payload !=null && !payload.equals(NullPayload.getInstance()) && !(payload instanceof NonDefinedPayload)){
+            message.setPayload(payload);
+        }
+
+        if ( returnMuleMessage.getInboundPropertyNames() != null && !returnMuleMessage.getInboundPropertyNames().isEmpty() ){
+            for ( String property : returnMuleMessage.getInboundPropertyNames() ){
+                message.setInboundProperty(property, returnMuleMessage.getInboundProperty(property));
+            }
+        }
+
+        if ( returnMuleMessage.getSessionPropertyNames() != null && !returnMuleMessage.getSessionPropertyNames().isEmpty() ){
+            for ( String property : returnMuleMessage.getSessionPropertyNames() ){
+                message.setSessionProperty(property, returnMuleMessage.getSessionProperty(property));
+            }
+        }
+
+        if ( returnMuleMessage.getInvocationPropertyNames() != null && !returnMuleMessage.getInvocationPropertyNames().isEmpty() ){
+            for ( String property : returnMuleMessage.getInvocationPropertyNames() ){
+                message.setInvocationProperty(property, returnMuleMessage.getInvocationProperty(property));
+            }
+        }
+
+        if ( returnMuleMessage.getOutboundPropertyNames() != null && !returnMuleMessage.getOutboundPropertyNames().isEmpty() ){
+            for ( String property : returnMuleMessage.getOutboundPropertyNames() ){
+                message.setOutboundProperty(property, returnMuleMessage.getOutboundProperty(property));
+            }
+        }
+
+    }
+
+    public static void verifyAssertions(MuleEvent event, List<MessageProcessor> assertions) {
+        if ( assertions == null ) return;
+
+        for ( MessageProcessor processor : assertions ){
+            try {
+                processor.process(event);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
