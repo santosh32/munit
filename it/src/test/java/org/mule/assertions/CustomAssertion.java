08374f0117885511a4b778aa6842fa2a1d4baed7
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-11-19 11:56:53 -0300

    fix build

--- it/src/test/java/org/mule/assertions/CustomAssertion.java
+++ it/src/test/java/org/mule/assertions/CustomAssertion.java
@@ -6,9 +6,9 @@ import org.mule.munit.MunitAssertion;
 
 public class CustomAssertion implements MunitAssertion{
     @Override
-    public MuleEvent execute(MuleEvent muleEvent) throws MunitAssertionError {
+    public MuleEvent execute(MuleEvent muleEvent) throws AssertionError {
         if ( !muleEvent.getMessage().getPayload().equals("Hello World") ){
-            throw new MunitAssertionError("Error the payload is incorrect");
+            throw new AssertionError("Error the payload is incorrect");
         }
 
         return muleEvent;
