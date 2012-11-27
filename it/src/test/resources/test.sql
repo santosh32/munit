dc1483ec38cdf5598f15a4c3ff3cff43accde9df
Javier Casal <javier.casal@mulesoft.com>
2012-11-27 15:27:32 -0300

    sql test file added

---
+++ it/src/test/resources/test.sql
@@ -0,0 +1,3 @@
+CREATE TABLE jobtitlelookup (jobtitle VARCHAR(255), eecsalaryorhourly VARCHAR(255), jobtitleid VARCHAR(255));
+INSERT INTO jobtitlelookup (jobtitle, eecsalaryorhourly, jobtitleid) VALUES ('Culinary Team Member', 'HOURLY', 3);
+INSERT INTO jobtitlelookup (jobtitle, eecsalaryorhourly, jobtitleid) VALUES ('Other', 'SALARY', 9);
