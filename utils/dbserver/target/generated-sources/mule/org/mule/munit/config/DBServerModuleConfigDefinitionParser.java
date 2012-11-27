04b0540b1179e942b87f77d5bf2e0ebc9c402ef9
Javier Casal <javier.casal@mulesoft.com>
2012-11-27 15:37:14 -0300

    creationalScript now is an sqlFile

--- utils/dbserver/target/generated-sources/mule/org/mule/munit/config/DBServerModuleConfigDefinitionParser.java
+++ utils/dbserver/target/generated-sources/mule/org/mule/munit/config/DBServerModuleConfigDefinitionParser.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
-@Generated(value = "Mule DevKit Version 3.3.1", date = "2012-11-23T12:55:32-03:00", comments = "Build 3.3.1.1298.3ae82a7")
+@Generated(value = "Mule DevKit Version 3.3.1", date = "2012-11-27T03:26:56-03:00", comments = "Build 3.3.1.1298.3ae82a7")
 public class DBServerModuleConfigDefinitionParser
     extends AbstractDefinitionParser
 {
@@ -21,7 +21,7 @@ public class DBServerModuleConfigDefinitionParser
         setInitMethodIfNeeded(builder, DBServerModuleProcessAdapter.class);
         setDestroyMethodIfNeeded(builder, DBServerModuleProcessAdapter.class);
         parseProperty(builder, element, "database", "database");
-        parseProperty(builder, element, "creationalScript", "creationalScript");
+        parseProperty(builder, element, "sqlFile", "sqlFile");
         parseProperty(builder, element, "csv", "csv");
         BeanDefinition definition = builder.getBeanDefinition();
         setNoRecurseOnDefinition(definition);
