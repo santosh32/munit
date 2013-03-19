2d948a5ef40ab56895d18c46c2f7d7677f87ae18
fernandofederico1984 <fernandofederico1984@gmail.com>
2013-03-19 14:06:32 -0300

    changed schema versions to 3.4

--- core/mock/src/main/java/org/mule/munit/MockModule.java
+++ core/mock/src/main/java/org/mule/munit/MockModule.java
@@ -29,7 +29,7 @@ import static org.mule.munit.common.MunitCore.buildMuleStackTrace;
  * @author Federico, Fernando
  * @version since 3.3.2
  */
-@Module(name="mock", schemaVersion="3.3")
+@Module(name="mock", schemaVersion="3.4")
 public class MockModule implements MuleContextAware
 {
     private MuleContext muleContext;
