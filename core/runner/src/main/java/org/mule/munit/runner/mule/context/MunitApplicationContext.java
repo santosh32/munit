e90e551e5710003cf9d2a07f3923268ed42bb395
fernandofederico1984 <fernandofederico1984@gmail.com>
2013-04-14 13:31:56 -0300

    configure MunitSpringFactoryPostProcessor bean definition with propertyValues, so setters are invoked when instantiating (cherry picked from commit 716debefd39d1e0d0bf2123f041aae25345acde8)

--- core/runner/src/main/java/org/mule/munit/runner/mule/context/MunitApplicationContext.java
+++ core/runner/src/main/java/org/mule/munit/runner/mule/context/MunitApplicationContext.java
@@ -1,24 +1,22 @@
 package org.mule.munit.runner.mule.context;
 
 import org.mule.api.MuleContext;
-import org.mule.api.config.MuleProperties;
 import org.mule.config.ConfigResource;
 import org.mule.config.spring.MissingParserProblemReporter;
 import org.mule.config.spring.MuleApplicationContext;
-import org.mule.munit.AssertModule;
 import org.mule.munit.common.connectors.ConnectorMethodInterceptorFactory;
 import org.mule.munit.common.endpoint.MunitSpringFactoryPostProcessor;
 import org.mule.munit.common.mp.MunitMessageProcessorInterceptorFactory;
+
+import java.io.IOException;
+
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.Resource;
-
-import java.io.IOException;
 
 
 public class MunitApplicationContext extends MuleApplicationContext{
@@ -46,11 +44,13 @@ public class MunitApplicationContext extends MuleApplicationContext{
         beanDefinitionReader.setProblemReporter(new MissingParserProblemReporter());
 
         if ( configuration != null ){
-            GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+            RootBeanDefinition beanDefinition = new RootBeanDefinition();
             beanDefinition.setBeanClass(MunitSpringFactoryPostProcessor.class);
-            beanDefinition.setAttribute("mockInbounds", configuration.isMockInbounds());
-            beanDefinition.setAttribute("mockConnectors", configuration.isMockConnectors());
-            beanDefinition.setAttribute("mockingExcludedFlows", configuration.getMockingExcludedFlows());
+            MutablePropertyValues propertyValues = new MutablePropertyValues();
+            propertyValues.add("mockInbounds", configuration.isMockInbounds());
+            propertyValues.add("mockConnectors", configuration.isMockConnectors());
+            propertyValues.add("mockingExcludedFlows", configuration.getMockingExcludedFlows());
+            beanDefinition.setPropertyValues(propertyValues);
             beanFactory.registerBeanDefinition("___MunitSpringFactoryPostProcessor", beanDefinition);
         }
         // Communicate mule context to parsers
